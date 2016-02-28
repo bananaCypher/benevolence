0f202b06f186549ba6d59ce430c14905a64a2920
Callum Hogg <bananaCypher@gmail.com>
2016-02-28 19:43:07 +0000

    Wrote some tests for USer

---
+++ Benevolence/spec/user/user_model_spec.rb
@@ -0,0 +1,20 @@
+require('rails_helper')
+
+describe User do
+  before(:each) do
+    @user = User.new(name: 'A User')
+  end
+
+  it "should have a name" do
+    pending
+  end
+  it "can have a profile picture" do
+    pending
+  end
+  it "should have a default profile picture if none is defined" do
+    pending
+  end
+  it "can have multiple uploaded songs" do
+
+  end
+end

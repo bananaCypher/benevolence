ad2473df9ee4fa41385dd1e2627b7cfa163850a6
Callum Hogg <bananaCypher@gmail.com>
2016-02-28 18:50:06 +0000

    Created more unit tests for Song

--- Benevolence/spec/song/song_model_spec.rb
+++ Benevolence/spec/song/song_model_spec.rb
@@ -2,9 +2,30 @@ require('rails_helper')
 
 describe Song do
   before(:each) do
-    @song = Song.create(title: 'My Song', duration: 180)
+    @song = Song.new(title: 'My Song', duration: 180)
+    @uploader = User.new()
+    @artist = Artist.new()
+    @song.user = @uploader
+    @song.artist = @artist
   end
 
+  it "should have a title" do
+    expect(@song.title).to eq('My Song')
+  end
+  it "should have a duration" do
+    expect(@song.duration).to eq(180)
+  end
+  it "should have an uploader" do
+    expect(@song.user.class.to_s).to eq('User')
+  end
+  it "should have an artist" do
+    expect(@song.artist.class.to_s).to eq('Artist')
+  end
+  it "can have an album" do
+    album = Album.new()
+    @song.album = album
+    expect(@song.album.class.to_s).to eq('Album')
+  end
   it "should return the correct file path for it's mp3" do
     expect(@song.file_path).to eq("/Users/User/Documents/Programming/Benevolence/Benevolence/public/tracks/#{@song.id}.mp3")
   end

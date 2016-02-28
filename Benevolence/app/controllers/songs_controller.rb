5d94e2603983ba85e6d0492f9882ea2afb203f41
Callum Hogg <bananaCypher@gmail.com>
2016-02-28 20:36:39 +0000

    Set up the controller actions for Song

--- Benevolence/app/controllers/songs_controller.rb
+++ Benevolence/app/controllers/songs_controller.rb
@@ -1,9 +1,29 @@
 class SongsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render json: Song.all, methods: :file_url
   end
 
   def show
     render json: Song.find(params[:id]), methods: :file_url
+  end
+
+  def create
+    song = Song.new(song_params)  
+    song.user = current_user
+    song.save
+    render json: song, methods: :file_url
+  end
+
+  def destroy
+    song = Song.find(params[:id])
+    song.destroy
+    render json: {status: 'success'}
+  end
+
+  private
+  def song_params
+    params.require(:song).permit(:title)
   end
 end

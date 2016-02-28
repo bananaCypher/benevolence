74bd50c3d489095637b5e17a1a9ca8aa3e999bf6
Callum Hogg <bananaCypher@gmail.com>
2016-02-28 19:39:09 +0000

    Completed the Album tests

--- Benevolence/spec/album/album_model_spec.rb
+++ Benevolence/spec/album/album_model_spec.rb
@@ -7,43 +7,31 @@ describe Album do
     @album.artist = @artist
     @song1 = Song.new()
     @song2 = Song.new()
-    field :name, type: String
-    field :year, type: Integer
-    field :track_list, type: Array
-    belongs_to :artist
-    has_many :songs
   end
 
   it "should have a name" do
-    expect(@artist.name).to eq('An Artist')
-  end
-  it "can have small art" do
-    @artist.small_art = @small_art
-    expect(@artist.small_art).to eq('http://lorempixel.com/200/200/')
-  end
-  it "can have large art" do
-    @artist.large_art = @large_art
-    expect(@artist.large_art).to eq('http://lorempixel.com/1000/600/')
-  end
-  it "should have a default small art if none given" do
-    expect(@artist.small_art).to eq(@default_small)
-  end
-  it "should have a default large art if none given" do
-    expect(@artist.large_art).to eq(@default_large)
-  end
-  it "can have biography" do
-    @artist.biography = @biography
-    expect(@artist.biography).to eq('This is a test artist.')
-  end
-  it "can have multiple albums" do
-    @artist.albums.push(@album1)
-    @artist.albums.push(@album2)
-    expect(@artist.albums.length).to eq(2)
-  end
-  it "can have multiple songs" do
-    @artist.songs.push(@song1)
-    @artist.songs.push(@song2)
-    expect(@artist.songs.length).to eq(2)
+    expect(@album.name).to eq('An Album')
   end
 
+  it "should have an artist" do
+    expect(@album.artist.class.to_s).to eq('Artist')
+  end
+
+  it "can have a year" do
+    year = 1993
+    @album.year = year
+    expect(@album.year).to eq(1993)
+  end
+
+  it "can have a track_list" do
+    track_list = ['song1', 'song2', 'song3']
+    @album.track_list = track_list
+    expect(@album.track_list.length).to eq(3)
+  end
+
+  it "can have multiple songs" do
+    @album.songs.push(@song1)
+    @album.songs.push(@song2)
+    expect(@album.songs.length).to eq(2)
+  end
 end

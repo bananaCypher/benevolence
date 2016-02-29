2213d3e98f5e38e46ef7fa0e5c9588021b45aa2a
Callum Hogg <bananaCypher@gmail.com>
2016-02-29 11:35:47 +0000

    Wrote the tests for Playlist show action

--- Benevolence/spec/controllers/playlists_controller_spec.rb
+++ Benevolence/spec/controllers/playlists_controller_spec.rb
@@ -22,4 +22,32 @@ RSpec.describe PlaylistsController, type: :controller do
     end
   end
 
+  describe 'GET show' do
+    before(:each) do
+      create(:playlist)
+      create(:playlist)
+      create(:playlist)
+      @playlist = create(:playlist) 
+    end
+    it 'should respond with JSON' do
+      get :show, {id: @playlist.id}
+      expect(response.content_type).to eq "application/json"
+    end
+    it 'should return the requested playlist' do
+      get :show, {id: @playlist.id}
+      expect(response.body).to match /"#{@playlist.id}"/
+    end
+    it 'should return only the requested playlist' do
+      get :show, {id: @playlist.id}
+      playlists = playlist.where(:id.ne => @playlist.id)
+      ids = playlists.map{|p| p.id }
+      ids.each do |id|
+        expect(response.body).to_not match /"#{id}\"}/
+      end
+    end
+    it "should return an error if the playlist isn't found" do
+      get :show, {id: 'iamsuchafakeid'}
+      expect(response.body).to match /"status":"error"/
+    end
+  end
 end

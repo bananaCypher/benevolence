2d053fbf8ae603da893e958ee8d28c2546e53fa2
Callum Hogg <bananaCypher@gmail.com>
2016-02-29 08:25:27 +0000

    Created an Artists controller and set up the routes

--- Benevolence/config/routes.rb
+++ Benevolence/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   scope '/api' do
     resources :songs
+    resources :artists
   end
 
   # The priority is based upon order of creation: first created -> highest priority.

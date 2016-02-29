53140e9ba7ed0ecc97400494700feb19818f6e67
Callum Hogg <bananaCypher@gmail.com>
2016-02-29 10:44:15 +0000

    Got the Album show action tests passing

--- Benevolence/app/controllers/albums_controller.rb
+++ Benevolence/app/controllers/albums_controller.rb
@@ -2,4 +2,20 @@ class AlbumsController < ApplicationController
   def index
     render json: Album.all
   end
+
+  def show
+    album = get_album || return
+    render json: album, methods: :file_url
+  end
+
+  private
+  def render_not_found
+    render json: {status: 'error', message: 'Album not found'}
+  end
+
+  def get_album
+    album = Album.find(params[:id])
+    render_not_found if !album
+    return album
+  end
 end

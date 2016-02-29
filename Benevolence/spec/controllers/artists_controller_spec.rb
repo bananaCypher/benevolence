26acccd2c78cf6c18f51d404849653db9dd0aedd
Callum Hogg <bananaCypher@gmail.com>
2016-02-29 08:28:27 +0000

    Created the tests for the Artist index action

--- Benevolence/spec/controllers/artists_controller_spec.rb
+++ Benevolence/spec/controllers/artists_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe ArtistsController, type: :controller do
-
+  describe 'GET index' do
+    before(:each) do
+      create(:artist)
+      create(:artist)
+      create(:artist)
+      create(:artist)
+      create(:artist)
+      create(:artist)
+    end
+    it 'should respond with JSON' do
+      get :index
+      expect(response.content_type).to eq "application/json"
+    end
+    it 'should contain all the artists' do
+      get :index
+      ids = Artist.all.map{|s| s.id }
+      ids.each do |id|
+        expect(response.body).to match /"#{id}\"}/
+      end
+    end
+  end
 end

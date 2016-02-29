5f20a8ff35305508cd43bab30d34d7c0e0c02b9c
Callum Hogg <bananaCypher@gmail.com>
2016-02-29 10:41:52 +0000

    Added the tests for Album show action

--- Benevolence/spec/controllers/albums_controller_spec.rb
+++ Benevolence/spec/controllers/albums_controller_spec.rb
@@ -21,4 +21,33 @@ RSpec.describe AlbumsController, type: :controller do
       end
     end
   end
+
+  describe 'GET show' do
+    before(:each) do
+      create(:album)
+      create(:album)
+      create(:album)
+      @album = create(:album) 
+    end
+    it 'should respond with JSON' do
+      get :show, {id: @album.id}
+      expect(response.content_type).to eq "application/json"
+    end
+    it 'should return the requested album' do
+      get :show, {id: @album.id}
+      expect(response.body).to match /"#{@album.id}"/
+    end
+    it 'should return only the requested album' do
+      get :show, {id: @album.id}
+      albums = Album.where(:id.ne => @album.id)
+      ids = albums.map{|a| a.id }
+      ids.each do |id|
+        expect(response.body).to_not match /"#{id}\"}/
+      end
+    end
+    it "should return an error if the album isn't found" do
+      get :show, {id: 'iamsuchafakeid'}
+      expect(response.body).to match /"status":"error"/
+    end
+  end
 end

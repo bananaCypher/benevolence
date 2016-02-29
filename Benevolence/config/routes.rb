05505f5e5d9582d00a73ce11a19d492c2878d301
Callum Hogg <bananaCypher@gmail.com>
2016-02-29 11:31:22 +0000

    Generated playlist controller

--- Benevolence/config/routes.rb
+++ Benevolence/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :songs
     resources :artists
     resources :albums
+    resources :playlists
   end
 
   # The priority is based upon order of creation: first created -> highest priority.

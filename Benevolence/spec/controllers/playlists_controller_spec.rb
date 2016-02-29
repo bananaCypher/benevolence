eabb0a62eff264124e9403e700d1af197d4efff0
Callum Hogg <bananaCypher@gmail.com>
2016-02-29 11:32:45 +0000

    Wrote tests for Playlist index action

--- Benevolence/spec/controllers/playlists_controller_spec.rb
+++ Benevolence/spec/controllers/playlists_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe PlaylistsController, type: :controller do
+  describe 'GET index' do
+    before(:each) do
+      create(:playlist)
+      create(:playlist)
+      create(:playlist)
+      create(:playlist)
+      create(:playlist)
+    end
+    it 'should respond with JSON' do
+      get :index
+      expect(response.content_type).to eq "application/json"
+    end
+    it 'should contain all the playlists' do
+      get :index
+      ids = Playlist.all.map{|p| p.id }
+      ids.each do |id|
+        expect(response.body).to match /"#{id}\"}/
+      end
+    end
+  end
 
 end

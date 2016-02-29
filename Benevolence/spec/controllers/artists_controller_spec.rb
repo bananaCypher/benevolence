9587227200b86d2808bf99313a4269e0f28580d2
Callum Hogg <bananaCypher@gmail.com>
2016-02-29 08:33:13 +0000

    Added all the tests for the Artist show action

--- Benevolence/spec/controllers/artists_controller_spec.rb
+++ Benevolence/spec/controllers/artists_controller_spec.rb
@@ -22,4 +22,33 @@ RSpec.describe ArtistsController, type: :controller do
       end
     end
   end
+  
+  describe 'GET show' do
+    before(:each) do
+      create(:artist)
+      create(:artist)
+      create(:artist)
+      @artist = create(:artist) 
+    end
+    it 'should respond with JSON' do
+      get :show, {id: @artist.id}
+      expect(response.content_type).to eq "application/json"
+    end
+    it 'should return the requested song' do
+      get :show, {id: @artist.id}
+      expect(response.body).to match /"#{@artist.id}"/
+    end
+    it 'should return only the requested song' do
+      get :show, {id: @artist.id}
+      artists = Artist.all[0..Artist.all.length - 2]
+      ids = artists.map{|s| s.id }
+      ids.each do |id|
+        expect(response.body).to_not match /"#{id}\"}/
+      end
+    end
+    it "should return an error if the song isn't found" do
+      get :show, {id: 'iamsuchafakeid'}
+      expect(response.body).to match /"status":"error"/
+    end
+  end
 end

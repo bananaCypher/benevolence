9fee7a600cca5998cc09ccf1791d8550fb0b0d00
Callum Hogg <bananaCypher@gmail.com>
2016-02-29 10:48:35 +0000

    Got all the Album create tests passing

--- Benevolence/app/controllers/albums_controller.rb
+++ Benevolence/app/controllers/albums_controller.rb
@@ -8,7 +8,21 @@ class AlbumsController < ApplicationController
     render json: album, methods: :file_url
   end
 
+  def create
+    begin
+      album = Album.new(album_params)  
+      album.save
+      render json: {status: 'success', message: 'Album was successfully created'}
+    rescue
+      render json: {status: 'error', message: 'Failed to create album'}
+    end
+  end
+
   private
+  def album_params
+    params.require(:album).permit(:name)
+  end
+
   def render_not_found
     render json: {status: 'error', message: 'Album not found'}
   end

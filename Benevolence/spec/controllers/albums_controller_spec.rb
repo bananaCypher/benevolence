b14b99e76e6cf5d83e83f89a92a87643e3957aa6
Callum Hogg <bananaCypher@gmail.com>
2016-02-29 10:52:39 +0000

    Created the tests for Album update action

--- Benevolence/spec/controllers/albums_controller_spec.rb
+++ Benevolence/spec/controllers/albums_controller_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe AlbumsController, type: :controller do
       @album_object = {album: {name: 'An Album'}}
     end
     it 'should respond with JSON' do
-      post :create, @artist_object
+      post :create, @album_object
       expect(response.content_type).to eq("application/json")
     end
     it 'should be able to create a new album' do
@@ -70,6 +70,38 @@ RSpec.describe AlbumsController, type: :controller do
     end
     it 'should return an error message if album failed to create' do
       post :create, {random_key: 'random stuff'}
+      expect(response.body).to match /"status":"error"/
+    end
+  end
+
+  describe 'PUT update' do
+    before(:each) do
+      @album = create(:album)
+      @update_object = {id: @album.id, album: {name: 'An updated Album'}}
+    end
+    it 'should respond with JSON' do
+      post :update, @update_object
+      expect(response.content_type).to eq("application/json")
+    end
+    it 'should update the given album' do
+      put :update, @update_object
+      album = Album.find(@album.id)
+      expect(album.name).to eq('An updated Album')
+    end
+    it 'should not update any other album' do
+      albums = Album.all.find_all {|a| a.id != @album.id}
+      put :update, @update_object
+      new_albums = Album.all.find_all {|a| a.id != @album.id}
+      new_albums.each_with_index do |a, i|
+        expect(a.name).to eq(albums[i].name)
+      end
+    end
+    it "should return a success message if the album was updated" do
+      put :update, @update_object
+      expect(response.body).to match /"status":"success"/
+    end
+    it "should return an error if the album isn't found" do
+      put :update, {id: 'fakeashellid', album: {name: 'An updated Album'}}
       expect(response.body).to match /"status":"error"/
     end
   end

8cdfd19b77dbcc00df84a9c832f86996aa374b15
Callum Hogg <bananaCypher@gmail.com>
2016-02-29 11:04:56 +0000

    Wrote some unit tests for the Playlist model

--- Benevolence/spec/models/playlist_spec.rb
+++ Benevolence/spec/models/playlist_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Playlist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @playlist = build(:playlist)
+    @playlist.user = build(:user)
+  end
+
+  it "should have a title" do
+    expect(@playlist.title).to eq('A Playlisy')
+  end
+  it "should have an owner" do
+    expect(@playlist.user.class.to_s).to eq('User')
+  end
+  it "can have multiple songs" do
+    @playlist.songs.push(build(:song_one))
+    @playlist.songs.push(build(:song_two))
+    expect(@playlist.songs.length).to eq(2)
+  end
 end

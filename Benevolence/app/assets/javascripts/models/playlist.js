ae25664361e0318952c963dcdcdc5ed925ca46e9
Callum Hogg <bananaCypher@gmail.com>
2016-02-29 14:53:39 +0000

    Playlist model now has playlist id, title and song list

--- Benevolence/app/assets/javascripts/models/playlist.js
+++ Benevolence/app/assets/javascripts/models/playlist.js
@@ -1,8 +1,25 @@
-var Playlist = function(){
+var baseURL = 'http://localhost:3000/api/playlists/'
 
+var Playlist = function(id){
+  this.id = id;
+  this.title = '';
+  this.tracks = [];
 }
 Playlist.prototype = {
-
+  get: function(playlistId){
+    var request = new XMLHttpRequest();
+    request.open('GET', baseURL + playlistId);
+    request.onload = function(){
+      if (request.status === 200) {
+        obj = JSON.parse(request.responsText); 
+        this.title = obj.title
+        this.tracks = obj.song_ids
+      } else {
+      }
+    }.bind(this);
+    request.send(null);
+    
+  }
 }
 
 try {
--- Benevolence/spec/javascript/models/playlist_specs.js
+++ Benevolence/spec/javascript/models/playlist_specs.js
@@ -2,9 +2,29 @@ var assert = require('assert');
 var Playlist = require('../../../app/assets/javascripts/models/playlist.js');
 
 describe('Playlist', function(){
-  it('should be able to get a playlist from the API', function(done){
-    var playlist = new Playlist()
-    playlist.get('56d4310afdc77eacb4000000') 
-    assert.equal(playlist.title, 'My Playlist')
+  beforeEach(function() {
+    //Can't do an AJAX request in mocha so manually setting up Playlist
+    playlist = new Playlist('56d4310afdc77eacb4000000');
+    playlist.title = 'My Playlist';
+    playlist.tracks = [
+      "56d43681fdc77eb085000002", 
+      "56d4369bfdc77eb085000003",
+      "56d436aafdc77eb085000004",
+      "56d436b7fdc77eb085000005",
+      "56d436bcfdc77eb085000006",
+      "56d436c5fdc77eb085000007",
+      "56d436cdfdc77eb085000008",
+      "56d436d6fdc77eb085000009",
+      "56d436e6fdc77eb08500000a" 
+    ]
+  });
+  it('should be have an id', function(){
+    assert.equal(playlist.id, '56d4310afdc77eacb4000000');
+  });
+  it('should be have a title', function(){
+    assert.equal(playlist.title, 'My Playlist');
+  });
+  it("should have a list of song id's", function(){
+    assert.equal(playlist.tracks.length, 9) ;
   });
 });

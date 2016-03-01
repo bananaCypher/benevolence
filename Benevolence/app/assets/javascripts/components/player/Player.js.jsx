c354b9c0e741a5cc816c9d433644bbfe8a8af505
Callum Hogg <bananaCypher@gmail.com>
2016-03-01 14:35:25 +0000

    Added a stop function and temporary button

--- Benevolence/app/assets/javascripts/components/player/Player.js.jsx
+++ Benevolence/app/assets/javascripts/components/player/Player.js.jsx
@@ -11,17 +11,28 @@ var Player = React.createClass({
       }); 
     }.bind(this));
   },
+  player: function() {
+    return document.getElementById('audio-player')
+  },
   play: function() {
-    document.getElementById('audio-player').play();
+    var player = this.player();
+    player.play();
   },
   pause: function() {
-    document.getElementById('audio-player').pause();
+    var player = this.player();
+    player.pause();
+  },
+  stop: function() {
+    var player = this.player();
+    player.pause();
+    player.currentTime = 0;
   },
   render: function() {
     return (
       <div>
       <PlayerButton action={this.play}>DJ Spin that Shit</PlayerButton>
       <PlayerButton action={this.pause}>DJ Pause that Sheet</PlayerButton> 
+      <PlayerButton action={this.stop}>DJ Stop that Sheit</PlayerButton> 
       <audio id='audio-player' src={this.state.songUrl}></audio>
       </div>
     );

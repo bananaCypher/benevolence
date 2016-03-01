829ae1c9aa8f7f8d1702448f6cbc6de181548e38
Callum Hogg <bananaCypher@gmail.com>
2016-03-01 10:34:14 +0000

    Created the get function on the JS Song model

--- Benevolence/app/assets/javascripts/models/song.js
+++ Benevolence/app/assets/javascripts/models/song.js
@@ -1,11 +1,32 @@
-var Song = function(){
-  
+var baseURL = 'http://localhost:3000/api/songs/'
+
+var Song = function(id){
+  this.id = id; 
+  this.title = '';
+  this.artist = '';
+  this.album = '';
+  this.url = '';
 }
 Song.prototype = {
-
+  get: function(callback){
+    var request = new XMLHttpRequest();
+    request.open('GET', baseURL + this.id);
+    request.onload = function(){
+      if (request.status === 200) {
+        obj = JSON.parse(request.responseText); 
+        this.title = obj.title;
+        this.artist = obj.artist;
+        if(obj.abum){ this.album=obj.album }
+        this.url = obj.file_url
+        callback(this);
+      } else {
+      }
+    }.bind(this);
+    request.send(null);
+  },
 }
 
 try {
-  module.exports = Playlist;
+  module.exports = Song;
 } 
 catch(err) {}
